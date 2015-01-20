85094aff0efbb6bcad4a173da1f98b191a776056
Faran Jessani <faran@faranjessani.com>
2015-01-20 18:12:55 -0500

    Assignment 2: Completed implementation of cache matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cached matrix is a matrix designed to cache
+## the expensive computation of inversion
+## It exposes a constructor function and
+## a varition of the solve method designed to achieve that end
 
-## Write a short comment describing this function
+## This constructor method takes a matrix as input and exposes methods to retrieve its fields
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function wraps the solve function with a simple cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
